1ca2bbe03846d345ff6c5ba149c1878abec8a58c
Azzrenoza <116853920+MathieuForel@users.noreply.github.com>
2023-02-17 11:02:43 +0100

    Push mecaniques termine

--- Assets/Level4/AdvancedLaser.cs
+++ Assets/Level4/AdvancedLaser.cs
@@ -6,6 +6,10 @@ public class AdvancedLaser : MonoBehaviour
 {
 
     public int MaxBounces = 5;
+    public int Y;
+
+    public bool Finish;
+
     public LineRenderer lr;
     public Transform startpoint;
 
@@ -19,13 +23,13 @@ public class AdvancedLaser : MonoBehaviour
 
     public void Update()
     {
-        CastLaser(transform.position, -transform.forward);
+        lr.positionCount = MaxBounces + 1;
+
+        CastLaser(transform.position, transform.forward);
     }
 
     public void CastLaser(Vector3 position, Vector3 direction)
     {
-        lr.SetPosition(0, startpoint.position);
-
         for(int i = 0; i < MaxBounces; i++)
         {
             Ray ray = new Ray(position, direction);
@@ -34,10 +38,37 @@ public class AdvancedLaser : MonoBehaviour
             if(Physics.Raycast(ray, out hit, 300, 1)) 
             {
                 Debug.Log("Raycast moment");
-
                 position = hit.point;
-                direction = Vector3.Reflect(direction, hit.normal);
-                lr.SetPosition(i+1, hit.point);
+
+                if (hit.transform.tag == "Mirror")
+                {
+                    direction = Vector3.Reflect(direction, hit.normal);
+                }
+                else
+                {
+                    while(i < MaxBounces)
+                    {
+                        lr.SetPosition(i + 1, hit.point);
+                        i++;
+                    }
+                }
+
+                if(hit.transform.tag == "Finish")
+                {
+                    Finish = true;
+                }
+                else
+                {
+                    Finish = false;
+                }
+
+                Y = MaxBounces - i;
+                while(Y > 0)
+                {
+                    Y--;
+
+                    lr.SetPosition(MaxBounces - Y, hit.point);
+                }
             }
         }
     }
