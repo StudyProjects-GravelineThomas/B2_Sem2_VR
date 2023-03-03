ed4b896ecc8567f9810d1bcc2578b8f6575b9782
Le_Thom <dev@lethom.fr>
2023-03-03 11:33:28 +0100

    Updated Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,8 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("AudioManager").GetComponent<AudioManager>().PlaySound("Menu");
+        GameObject.Find("AudioManager").GetComponent<AudioManager>().PlaySound("ButtonClick");
+        GameObject.Find("SceneManager").GetComponent<SceneManager>().LoadScene("Hugo_Lvl1");
     }
 
     // Update is called once per frame
