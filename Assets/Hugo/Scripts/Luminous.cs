1b35339a28151f55b14d633d80919f500c718d0f
Hugo Carrasco <carrasco.hugo.hc@gmail.com>
2023-03-10 12:22:18 +0100

    Suppression de Octane, ajout des chemins lumineux et du code

---
+++ Assets/Hugo/Scripts/Luminous.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Luminous : MonoBehaviour
+{
+    public GameObject PressurePlate_Luminous;
+    public GameObject Key_Luminous;
+    public GameObject Potion_Luminous;
+    public GameObject FinalPath_Luminous;
+    public int WhichOne =0;
+
+    public void LuminousProtocol()
+    {
+        if(WhichOne ==0)
+        {
+            PressurePlate_Luminous.SetActive(true);
+            WhichOne+=1;
+        }
+        if(WhichOne ==1)
+        {
+            PressurePlate_Luminous.SetActive(false);
+            Key_Luminous.SetActive(true);
+            WhichOne+=1;
+        }
+        if(WhichOne ==2)
+        {
+            Key_Luminous.SetActive(false);
+            Potion_Luminous.SetActive(true);
+            WhichOne+=1;
+        }
+        if(WhichOne ==3)
+        {
+            Potion_Luminous.SetActive(false);
+            FinalPath_Luminous.SetActive(true);
+            WhichOne+=1;
+        }
+        
+    }
+
+}
